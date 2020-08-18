5233206109d4234132f6d92fbf77f36ca4066d8e
Alexandru Burghelea <burghelea.alex@gmail.com>
2020-08-18 22:53:47 +0300

    added api call

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-console.log('testing');
+//http://api.forismatic.com/api/1.0/
+
+//get Quote From api
+
+async function getQuate(){
+
+    const proxyUrl = 'https://young-sierra-21800.herokuapp.com/';
+    const method = 'getQuote';
+    const lang = 'en';
+    const format ='json'
+    const apiUrl = `http://api.forismatic.com/api/1.0/?method=${method}&lang=${lang}&format=${format}`;
+
+    try{
+        const response = await fetch(proxyUrl+apiUrl);
+        const data = await response.json();
+        console.log(data);
+        
+    }catch(error){
+        getQuate();
+        console.log(' whoops, no quote',error);
+    }
+
+}
+
+
+//on load
+getQuate();
